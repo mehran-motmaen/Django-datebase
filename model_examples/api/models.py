877bb1754348a9b6143f778e8ccce4e66817993d
motmaen <M5566M>
2022-12-13 18:16:46 +0330

    initiate app

--- model_examples/api/models.py
+++ model_examples/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Simple(models.Model):
+    text = models.CharField(max_length=10)
+    numbers = models.IntegerField(null=True)
+    url = models.URLField(default='example.com')
+
+    def __str__(self):
+        return self.url
